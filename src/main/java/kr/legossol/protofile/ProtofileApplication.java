4a365997cb1b8d395f5e451ea22d7127e47024a0
장해솔 <hsjang@iparking.co.kr>
2022-05-23 08:27:14 +0900

    first commit

---
+++ src/main/java/kr/legossol/protofile/ProtofileApplication.java
@@ -0,0 +1,13 @@
+package kr.legossol.protofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtofileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtofileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/legossol/protofile/ProtofileApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.legossol.protofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
